d564381352c6bd34ecae98ee46ae64a998786d84
Cui Shujun <cui.shujun@bsp.com>
2020-03-02 15:24:37 +0800

    Initial commit

---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.atguigu.java;
+
+//注释
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    public void test(){
+
+    }
+    /**
+    *客户的id
+    */
+    private int id = 2;
+    /**
+     *客户的姓名
+     */
+    private String name;
+    
+}
